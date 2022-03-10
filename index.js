e3ab8efefd0fb74cb198dbfd617a1a7b44adb914
BenjaminChun <chunweijiebenjamin@gmail.com>
2022-03-10 15:06:14 +0800

    update from week ago

--- index.js
+++
@@ -1,7 +0,0 @@
-const navLinks = document.querySelectorAll('.nav_links')
-
-navLinks.forEach(link=> {
-    link.addEventListener('click',()=>{
-        document.body.classList.
-    })
-})
---
+++ script.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $('#button').click(function(){
+        $('#txt1').val('Button clicked');
+    });
+});
